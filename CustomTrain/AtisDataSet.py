569e5c3be74ff76b14158332244ec4b370d04c8a
emreatasoy <memreatasoy@gmail.com>
2021-05-07 01:39:00 +0300

    Add files via upload
    first commit

---
+++ CustomTrain/AtisDataSet.py
@@ -0,0 +1,62 @@
+import os
+import torch
+import torch.utils.data
+from pathlib import Path
+from transformers import DistilBertTokenizerFast
+from sklearn.model_selection import train_test_split
+
+class AtisDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+def read_imdb_split(split_dir):
+    split_dir = Path(split_dir)
+    texts = []
+    labels = []
+
+    text_file = "seq.in"
+    label_file = 'label'
+
+    with open(os.path.join(split_dir, text_file)) as f:
+        for x in f:
+            texts.append(x.rstrip("\n"))
+    f.close()
+
+
+    with open(os.path.join(split_dir, label_file)) as f:
+        for x in f:
+            labels.append(x.rstrip("\n"))
+    f.close()
+
+
+    return texts, labels
+
+train_texts, train_labels = read_imdb_split('atis/train')
+# print(train_texts)
+# print(train_labels)
+
+test_texts, test_labels = read_imdb_split('atis/test')
+
+# print(test_texts)
+# print(test_labels)
+
+train_texts, val_texts, train_labels, val_labels = train_test_split(train_texts, train_labels, test_size=.2)
+
+tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+
+train_encodings = tokenizer(train_texts, truncation=True, padding=True)
+val_encodings = tokenizer(val_texts, truncation=True, padding=True)
+test_encodings = tokenizer(test_texts, truncation=True, padding=True)
+
+train_dataset = AtisDataset(train_encodings, train_labels)
+val_dataset = AtisDataset(val_encodings, val_labels)
+test_dataset = AtisDataset(test_encodings, test_labels)
---
+++ CustomTrain/main.py
@@ -0,0 +1,35 @@
+import torch
+from AtisDataSet import *
+
+# Bu data okumak icin
+from torch.utils.data import DataLoader
+
+#Bu fine-tune edeceğimiz modeli indiriyor.
+from transformers import DistilBertForSequenceClassification, AdamW
+
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+#Burada bir de class sayısının verilmesi gerekiyor argüman olarak. Ör: num_labels=5
+model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased')
+
+model.to(device)
+
+#Bu modelin train moda girmesini sağlıyor.
+model.train()
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+optim = AdamW(model.parameters(), lr=5e-5)
+
+for epoch in range(3):
+    for batch in train_loader:
+        optim.zero_grad()
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs[0]
+        loss.backward()
+        optim.step()
+
+model.eval()
